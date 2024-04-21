24625799f60c931cf4d2663625f29f4230ce0303
SohailSajid <sohailsajid79@gmail.com>
2024-04-21 23:06:01 +0100

    modified html links, deployed storage & function

--- frontend/main.js
+++ frontend/main.js
@@ -2,14 +2,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = 'http://localhost:7071/api/GetAndUpdateCounter';
+const functionProdURL = "https://resfunctioncapp.azurewebsites.net/api/GetAndUpdateCounter?code=OwApB-yEglihIvBK7nQ2Om2OfrkFONh7SG9vJ5CyWPJxAzFuQs2Gsg=="
+
+const functionDevURL = 'http://localhost:7071/api/GetAndUpdateCounter';
 
 const getVisitCount = async () => {
     let count = 0;
     try {
-        const response = await fetch(functionApi);
+        const response = await fetch(functionProdURL);
         const res = await response.json();
-        console.log('calling function API');
+        // console.log('calling function API');
         count = res.count;
         document.getElementById('counter').innerText = count;
     } catch (error) {
